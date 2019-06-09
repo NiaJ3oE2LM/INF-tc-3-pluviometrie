8dd26655b1d5aab99afad961bf7eb1cc87f2d833
phrygoh <paulndlc@gmail.com>
2019-06-09 17:36:33 +0200

    je réessye de faire le serveur

--- database_serveur0.py
+++ database_serveur0.py
@@ -3,7 +3,7 @@ import socketserver
 
 class RequestHandler(http.server.SimpleHTTPRequestHandler):
     # sous-répertoire racine des documents statiques  
-    static_dir = 'C:\\Users\\pauln\\Documents\\Python_Scripts\\info\\appln web\\projet\\INF-tc-3-pluviometrie\\client'# on surcharge la méthode qui traite les requêtes GET  
+    static_dir = '/client'# on surcharge la méthode qui traite les requêtes GET  
     def do_GET(self):# on modifie le chemin d'accès en insérant un répertoire préfixe
         self.path = self.static_dir + self.path# on traite la requête via la classe parent
         http.server.SimpleHTTPRequestHandler.do_GET(self)
