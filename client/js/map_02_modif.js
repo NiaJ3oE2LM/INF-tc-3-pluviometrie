dbe0026f6f8dfadf0d6dd53b8b38d5a267d275ca
Pierre-Olivier <becourt.pierreolivier@gmail.com>
2019-06-13 11:38:07 +0200

    Update map_02_modif.js

--- client/js/map_02_modif.js
+++ client/js/map_02_modif.js
@@ -16,7 +16,7 @@
           locations.push(['<div class="infobox"><h3 class="title"><a href="#">'+String(data[id].nom)+'</a></h3><span>blabla</span><span>blabla2</span>',data[id].lat,data[id].lon])
   	    }//locations=[[nom,lat,long],[],...]
       };
-      setTimeout(function(){map(locations);},100);
+      setTimeout(function(){map(locations);},1000);
     })(jQuery);
     //   //construction de la carte
     function map(locations) {
